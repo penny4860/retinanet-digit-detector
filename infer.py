91171ee0e705ee2694dcc96f98e22a6d454baa4d
jeongjoonsup <penny4860@gmail.com>
2019-05-11 16:37:23 +0900

    code cleaning

--- infer.py
+++ infer.py
@@ -1,10 +1,9 @@
+# -*- coding: utf-8 -*-
+
 import keras
 
-# import keras_retinanet
 from keras_retinanet import models
 from keras_retinanet.utils.image import read_image_bgr, preprocess_image, resize_image
-from keras_retinanet.utils.visualization import draw_box, draw_caption
-from keras_retinanet.utils.colors import label_color
 
 # import miscellaneous modules
 import matplotlib.pyplot as plt
@@ -13,44 +12,8 @@ import os
 import numpy as np
 import time
 
-# set tf backend to allow memory to grow, instead of claiming everything
-import tensorflow as tf
-
-def get_session():
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    return tf.Session(config=config)
-
-
-def visualize(boxes, scores, labels, scale,
-              labels_to_names = {0: '10', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9'}):
-    # correct for image scale
-    boxes /= scale
-    
-    # visualize detections
-    for box, score, label in zip(boxes[0], scores[0], labels[0]):
-        # scores are sorted so we can break
-        if score < 0.5:
-            break
-            
-        color = label_color(label)
-        
-        b = box.astype(int)
-        draw_box(draw, b, color=color)
-        
-        caption = "{} {:.3f}".format(labels_to_names[label], score)
-        draw_caption(draw, b, caption)
-        
-    plt.figure(figsize=(15, 15))
-    plt.axis('off')
-    plt.imshow(draw)
-    plt.show()
-
 
 if __name__ == '__main__':
-    # set the modified tf session as backend in keras
-    keras.backend.tensorflow_backend.set_session(get_session())
-    
     # adjust this to point to your downloaded/trained model
     # models can be downloaded here: https://github.com/fizyr/keras-retinanet/releases
     model_path = os.path.join('snapshots', 'resnet.h5')
@@ -79,8 +42,24 @@ if __name__ == '__main__':
     boxes, scores, labels = model.predict_on_batch(np.expand_dims(image, axis=0))
     print("processing time: ", time.time() - start)
     
-    visualize(boxes, scores, labels, scale)
-
+    #################################################
+    # post-processing
+    h, w, _ = image.shape
+    h2, w2, _ = draw.shape
+    boxes[:, :, 0] = boxes[:, :, 0] / w * w2
+    boxes[:, :, 2] = boxes[:, :, 2] / w * w2
+    boxes[:, :, 1] = boxes[:, :, 1] / h * h2
+    boxes[:, :, 3] = boxes[:, :, 3] / h * h2
+    labels = labels[0]
+    scores = scores[0]
+    boxes = boxes[0]
+    #################################################
     
+    from utils import visualize_boxes
+    visualize_boxes(draw, boxes, labels, scores, class_labels=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
+    
+    # 5. plot    
+    plt.imshow(draw)
+    plt.show()
 
 

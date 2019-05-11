67ad4d8737d4fd5c228e56603df15eacd4661483
jeongjoonsup <penny4860@gmail.com>
2019-05-11 16:54:02 +0900

    post_process() extracted

--- infer.py
+++ infer.py
@@ -11,22 +11,32 @@ import time
 
 from retina.utils import visualize_boxes
 
+MODEL_PATH = 'snapshots/resnet.h5'
+IMAGE_PATH = 'samples/JPEGImages/2.png'
 
-if __name__ == '__main__':
-    # adjust this to point to your downloaded/trained model
-    # models can be downloaded here: https://github.com/fizyr/keras-retinanet/releases
-    model_path = os.path.join('snapshots', 'resnet.h5')
-    
-    # load retinanet model
+def load_inference_model(model_path=os.path.join('snapshots', 'resnet.h5')):
     model = models.load_model(model_path, backbone_name='resnet50')
-    
-    # if the model is not converted to an inference model, use the line below
-    # see: https://github.com/fizyr/keras-retinanet#converting-a-training-model-to-inference-model
     model = models.convert_model(model)
     model.summary()
+    return model
+
+def post_process(boxes, original_img, preprocessed_img):
+    # post-processing
+    h, w, _ = preprocessed_img.shape
+    h2, w2, _ = original_img.shape
+    boxes[:, :, 0] = boxes[:, :, 0] / w * w2
+    boxes[:, :, 2] = boxes[:, :, 2] / w * w2
+    boxes[:, :, 1] = boxes[:, :, 1] / h * h2
+    boxes[:, :, 3] = boxes[:, :, 3] / h * h2
+    return boxes
+
+
+if __name__ == '__main__':
+    
+    model = load_inference_model(MODEL_PATH)
     
     # load image
-    image = read_image_bgr('samples/JPEGImages/2.png')
+    image = read_image_bgr(IMAGE_PATH)
     
     # copy to draw on
     draw = image.copy()
@@ -34,25 +44,17 @@ if __name__ == '__main__':
     
     # preprocess image for network
     image = preprocess_image(image)
-    image, scale = resize_image(image)
+    image, _ = resize_image(image)
     
     # process image
     start = time.time()
     boxes, scores, labels = model.predict_on_batch(np.expand_dims(image, axis=0))
     print("processing time: ", time.time() - start)
     
-    #################################################
-    # post-processing
-    h, w, _ = image.shape
-    h2, w2, _ = draw.shape
-    boxes[:, :, 0] = boxes[:, :, 0] / w * w2
-    boxes[:, :, 2] = boxes[:, :, 2] / w * w2
-    boxes[:, :, 1] = boxes[:, :, 1] / h * h2
-    boxes[:, :, 3] = boxes[:, :, 3] / h * h2
+    boxes = post_process(boxes, draw, image)
     labels = labels[0]
     scores = scores[0]
     boxes = boxes[0]
-    #################################################
     
     visualize_boxes(draw, boxes, labels, scores, class_labels=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
     
